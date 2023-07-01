2e7bbbb8cdc5c9b492bb74ce50a0cbae147ccf1f
OneSwordFT <yushenlee@163.com>
2023-07-01 20:12:36 +0800

    01笔记，通过写注释系统学习spring工程

--- 01.Start-Spring-Boot/src/main/java/com/springboot/demo/DemoApplication.java
+++ 01.Start-Spring-Boot/src/main/java/com/springboot/demo/DemoApplication.java
@@ -5,16 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/* springboot的@RestController和主应用@SpringBootApplication放在一起。。。不太好吧 */
+
 @RestController
 @SpringBootApplication
 public class DemoApplication {
 
-	@RequestMapping("/")
-	String index() {
-		return "hello spring boot";
-	}
+    @RequestMapping("/")
+    String index() {
+        return "hello spring boot";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
